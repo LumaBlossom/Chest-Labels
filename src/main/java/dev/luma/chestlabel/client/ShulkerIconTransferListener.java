efd13006ee19debccc86fccb3c81ba876b0423dc
Orb4t3l <trevducket@gmail.com>
2026-07-13 14:14:12 -0700

    updated the shulker icon transfer listener

--- src/main/java/dev/luma/chestlabel/client/ShulkerIconTransferListener.java
+++ src/main/java/dev/luma/chestlabel/client/ShulkerIconTransferListener.java
@@ -2,10 +2,12 @@ package dev.luma.chestlabel.client;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,6 +19,8 @@ public class ShulkerIconTransferListener {
     private static final String ICON_TAG_KEY = "chestlabel_icon_carry";
 
     private static ItemStack lastPlacingStack = ItemStack.EMPTY;
+    private static BlockPos pendingBreakPos;
+    private static ItemStack pendingBreakIcon = ItemStack.EMPTY;
 
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
@@ -30,27 +34,48 @@ public class ShulkerIconTransferListener {
     }
 
     @SubscribeEvent
-    public static void onHarvestDrops(BlockEvent.HarvestDropsEvent event) {
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
         if (!(event.getState().getBlock() instanceof ShulkerBoxBlock)) {
             return;
         }
-        BlockPos pos = event.getPos();
-        BlockEntity be = event.getLevel().getBlockEntity(pos);
+        BlockEntity be = event.getLevel().getBlockEntity(event.getPos());
         if (be == null) {
+            pendingBreakPos = null;
+            pendingBreakIcon = ItemStack.EMPTY;
             return;
         }
         ItemStack icon = ShulkerLabelStorage.getLogoItem(be);
         if (icon.isEmpty()) {
+            pendingBreakPos = null;
+            pendingBreakIcon = ItemStack.EMPTY;
             return;
         }
-        for (ItemStack drop : event.getDrops()) {
-            if (drop.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof ShulkerBoxBlock) {
-                CompoundTag iconTag = new CompoundTag();
-                icon.save(iconTag);
-                drop.getOrCreateTag().put(ICON_TAG_KEY, iconTag);
-                break;
-            }
+        pendingBreakPos = event.getPos().immutable();
+        pendingBreakIcon = icon;
+    }
+
+    @SubscribeEvent
+    public static void onEntityJoinLevel(EntityJoinLevelEvent event) {
+        if (pendingBreakPos == null || pendingBreakIcon.isEmpty()) {
+            return;
         }
+        if (!(event.getEntity() instanceof ItemEntity itemEntity)) {
+            return;
+        }
+        ItemStack stack = itemEntity.getItem();
+        if (!(stack.getItem() instanceof BlockItem blockItem) || !(blockItem.getBlock() instanceof ShulkerBoxBlock)) {
+            return;
+        }
+        BlockPos entityPos = itemEntity.blockPosition();
+        if (entityPos.distSqr(pendingBreakPos) > 4) {
+            return;
+        }
+        CompoundTag iconTag = new CompoundTag();
+        pendingBreakIcon.save(iconTag);
+        stack.getOrCreateTag().put(ICON_TAG_KEY, iconTag);
+
+        pendingBreakPos = null;
+        pendingBreakIcon = ItemStack.EMPTY;
     }
 
     @SubscribeEvent
